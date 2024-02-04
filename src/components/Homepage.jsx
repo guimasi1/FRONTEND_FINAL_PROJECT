4b05caf3d818a631e03f9320d27d16a8c081d889
guimasi1 <143355576+guimasi1@users.noreply.github.com>
2024-02-04 10:44:00 +0100

    components folder, navbar

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,13 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+const Homepage = () => {
+  return (
+    <Container>
+      <Row>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Homepage;

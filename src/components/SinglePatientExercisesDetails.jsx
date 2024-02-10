b0a847420007e3139e1799c4bdadcc949526d828
guimasi1 <143355576+guimasi1@users.noreply.github.com>
2024-02-10 23:47:19 +0100

    style change

--- src/components/SinglePatientExercisesDetails.jsx
+++ src/components/SinglePatientExercisesDetails.jsx
@@ -1,5 +1,5 @@
 import { Col, Row } from "react-bootstrap";
-
+import { motion } from "framer-motion";
 const SinglePatientExercisesDetails = ({ exercise }) => {
   return (
     <Row className="pt-4">
@@ -7,22 +7,40 @@ const SinglePatientExercisesDetails = ({ exercise }) => {
         <img src="https://placekitten.com/70" className="rounded-4" alt="" />
       </Col>
       <Col className="p-0 fw-bold" xs={5}>
-        {exercise.exercise.name}
+        {exercise.exercise && exercise.exercise.name}
       </Col>
       <Col
-        xs={2}
+        xs={1}
         className="p-0 d-flex flex-column justify-content-center align-items-center gap-1"
       >
-        <strong>Sets</strong> <p>{exercise.sets}</p>
+        <strong>Sets</strong> <p>{exercise && exercise.sets}</p>
       </Col>
       <Col
-        xs={2}
+        xs={1}
         className="p-0 d-flex flex-column justify-content-center align-items-center gap-1"
       >
-        <strong>Reps</strong> <p>{exercise.reps}</p>
+        <strong>Reps</strong> <p>{exercise && exercise.reps}</p>
       </Col>
-      <Col xs={12} className="mt-3 border-bottom pb-4">
-        {exercise.exercise.description}
+      <Col
+        xs={1}
+        className="p-0 d-flex flex-column justify-content-center align-items-end gap-1 flex-grow-1 text-end"
+      >
+        <p className="p-0 me-4">
+          <span
+            className={`mb-3 fs-1 fw-bold material-symbols-outlined ${
+              exercise.exercise && exercise.exercise.difficultyLevel === "EASY"
+                ? "text-success"
+                : exercise.exercise.difficultyLevel === "MEDIUM"
+                ? "text-warning"
+                : "text-danger"
+            }`}
+          >
+            circle
+          </span>
+        </p>
+      </Col>
+      <Col xs={12} className="mt-3 border-bottom pb-4 description-paragraph">
+        {exercise.exercise && exercise.exercise.description}
       </Col>
     </Row>
   );

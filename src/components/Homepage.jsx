6cab5dbb8adb7848a8ccdc37af2f107859b627dc
guimasi1 <143355576+guimasi1@users.noreply.github.com>
2024-02-25 22:54:00 +0100

    adds check svg

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -436,6 +436,39 @@ const Homepage = () => {
       </Row>
 
       <Row className="" id="row-margin-top"></Row>
+      <Row className="mt-5">
+        <Col className="d-flex flex-column text-center mt-5">
+          <h4 className="fs-1 fw-bold">Are you ready?</h4>
+          <div className="d-flex justify-content-center ">
+            <svg
+              className="w-25"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                <motion.path
+                  d="M8 12.3333L10.4615 15L16 9M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+                  stroke="#0e9a3d"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  initial={{ opacity: 0, pathLength: 0 }}
+                  animate={{ opacity: 1, pathLength: 1 }}
+                  whileInView={{ opacity: 1, pathLength: 1 }}
+                  transition={{ duration: 2.5, delay: 4 }}
+                ></motion.path>
+              </g>
+            </svg>
+          </div>
+        </Col>
+      </Row>
       <GoUpButton />
     </Container>
   );
